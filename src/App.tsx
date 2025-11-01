3cc7416fb7444993b3c1689cb52bdebeac4ff725
juanchoalcoba <juanchoalcoba@gmail.com>
2025-11-01 05:47:08 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ export default function App() {
   return (
     <>
     <Navbar />
-    <div className="min-h-screen bg-black text-white pt-24 flex items-center justify-center px-6">
+    <div className="min-h-screen bg-black text-white pt-24 flex items-center justify-center overflow-x-hidden px-6">
       <div className="max-w-7xl mx-auto flex md:flex-row gap-24 items-center flex-col-reverse">
         {/* Text */}
-        <div className="">
-          <h1 className="text-5xl md:text-6xl w-125 font-extrabold mb-6 leading-tight">
+        <div className="w-96 md:w-138">
+          <h1 className="text-5xl md:text-6xl font-extrabold mb-6 leading-tight">
             Bienvenidos a <span className="text-blue-400">Olga</span> en Vivo
           </h1>
-          <p className="text-lg text-gray-300 mb-8 w-125">
+          <p className="text-lg text-gray-300 mb-8">
             El nuevo hogar digital de la radio más innovadora de Argentina.
             Conectate, mirá y viví Olga en una experiencia web de otro nivel.
           </p>
